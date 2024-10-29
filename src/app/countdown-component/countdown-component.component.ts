2fcc6d0bd73b32a0071d43847eb802979884941f
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-10-28 21:31:26 -0600

    countdown

--- src/app/countdown-component/countdown-component.component.ts
+++ src/app/countdown-component/countdown-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { differenceInDays, differenceInHours, differenceInMinutes, differenceInSeconds } from 'date-fns';
 
 @Component({
   selector: 'app-countdown',
@@ -6,29 +7,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './countdown-component.component.html',
   styleUrls: ['./countdown-component.component.scss']
 })
-export class CountdownComponent{
-  // Variables para mostrar días, horas, minutos y segundos
+export class CountdownComponent implements OnInit, OnDestroy {
   days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
 
-  targetDate: Date = new Date('2024-12-25T00:00:00'); // Fecha objetivo (puedes cambiarla)
+  targetDate: Date = new Date('2025-02-23T00:00:00'); // Nueva fecha objetivo
+  private intervalId: any;
 
   ngOnInit() {
     this.updateCountdown();
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.updateCountdown();
     }, 1000);
   }
 
   updateCountdown() {
-    const now = new Date().getTime();
-    const distance = this.targetDate.getTime() - now;
+    const now = new Date();
+    const distance = this.targetDate.getTime() - now.getTime();
 
-    this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (distance > 0) {
+      this.days = differenceInDays(this.targetDate, now);
+      this.hours = differenceInHours(this.targetDate, now) % 24;
+      this.minutes = differenceInMinutes(this.targetDate, now) % 60;
+      this.seconds = differenceInSeconds(this.targetDate, now) % 60;
+    } else {
+      // Si la cuenta regresiva ha terminado
+      this.days = this.hours = this.minutes = this.seconds = 0;
+      clearInterval(this.intervalId); // Detener el intervalo
+    }
+  }
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
   }
 }
