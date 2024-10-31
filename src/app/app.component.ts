802a4ef7a41d251b985f75593d1eeba27d5e3149
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-10-30 22:51:22 -0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,29 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 // import { SimpleComponent } from './simple/simple.component';
 import { CountdownComponent } from './countdown-component/countdown-component.component';
+import { LocationMapComponent } from './location-map/location-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CountdownComponent],
+  imports: [RouterOutlet, CommonModule, CountdownComponent, LocationMapComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Fecha';
+  
+}
+function scheduleReminder() {
+    
+}
+
+function seeLocation(){
+
+}
+function verDatos(){
+
+}
+function SendReply(){
+
 }
