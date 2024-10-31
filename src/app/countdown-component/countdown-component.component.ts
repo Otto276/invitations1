ee4009b553cb66a68736a60c7a805f94ad44aa5c
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-10-30 22:51:41 -0600

    fixCountDown

--- src/app/countdown-component/countdown-component.component.ts
+++ src/app/countdown-component/countdown-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { differenceInDays, differenceInHours, differenceInMinutes, differenceInSeconds } from 'date-fns';
 
 @Component({
@@ -8,37 +8,49 @@ import { differenceInDays, differenceInHours, differenceInMinutes, differenceInS
   styleUrls: ['./countdown-component.component.scss']
 })
 export class CountdownComponent implements OnInit, OnDestroy {
+  targetDate: Date = new Date('2025-02-23T17:00:00Z');
+  intervalId: any;
   days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
 
-  targetDate: Date = new Date('2025-02-23T00:00:00'); // Nueva fecha objetivo
-  private intervalId: any;
+  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.updateCountdown();
-    this.intervalId = setInterval(() => {
-      this.updateCountdown();
-    }, 1000);
+    this.ngZone.runOutsideAngular(() => {
+      this.intervalId = setInterval(() => {
+        this.ngZone.run(() => this.updateCountdown());
+      }, 1000);
+    });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
   }
 
   updateCountdown() {
     const now = new Date();
     const distance = this.targetDate.getTime() - now.getTime();
-
     if (distance > 0) {
       this.days = differenceInDays(this.targetDate, now);
       this.hours = differenceInHours(this.targetDate, now) % 24;
       this.minutes = differenceInMinutes(this.targetDate, now) % 60;
       this.seconds = differenceInSeconds(this.targetDate, now) % 60;
     } else {
-      // Si la cuenta regresiva ha terminado
-      this.days = this.hours = this.minutes = this.seconds = 0;
-      clearInterval(this.intervalId); // Detener el intervalo
+      this.resetCountdown();
     }
+    this.cdr.detectChanges(); // Agrega esto para que el cambio se detecte y se actualice la vista
   }
-  ngOnDestroy() {
+
+  resetCountdown() {
+    this.days = this.hours = this.minutes = this.seconds = 0;
     clearInterval(this.intervalId);
+  }
+
+  setTargetDate(newDate: Date) {
+    this.targetDate = newDate;
+    this.updateCountdown();
   }
 }
