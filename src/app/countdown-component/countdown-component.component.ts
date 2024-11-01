186faa47b9500cace6de0d2857e6c910e9f9c45f
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-10-31 21:06:43 -0600

    fixCountdown

--- src/app/countdown-component/countdown-component.component.ts
+++ src/app/countdown-component/countdown-component.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import{ Component, OnDestroy, OnInit } from '@angular/core';
 import { differenceInDays, differenceInHours, differenceInMinutes, differenceInSeconds } from 'date-fns';
 
 @Component({
   selector: 'app-countdown',
   standalone: true,
   templateUrl: './countdown-component.component.html',
-  styleUrls: ['./countdown-component.component.scss']
+  styleUrls: ['./countdown-component.component.scss'],
 })
 export class CountdownComponent implements OnInit, OnDestroy {
   targetDate: Date = new Date('2025-02-23T17:00:00Z');
@@ -14,34 +14,39 @@ export class CountdownComponent implements OnInit, OnDestroy {
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
-
-  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
+  private destroyComponent: boolean = false;
 
   ngOnInit() {
-    this.updateCountdown();
-    this.ngZone.runOutsideAngular(() => {
-      this.intervalId = setInterval(() => {
-        this.ngZone.run(() => this.updateCountdown());
-      }, 1000);
-    });
+    this.updateCountdown().then(()=>{});
   }
 
   ngOnDestroy() {
     clearInterval(this.intervalId);
+    this.destroyComponent = true;
   }
 
-  updateCountdown() {
-    const now = new Date();
-    const distance = this.targetDate.getTime() - now.getTime();
-    if (distance > 0) {
-      this.days = differenceInDays(this.targetDate, now);
-      this.hours = differenceInHours(this.targetDate, now) % 24;
-      this.minutes = differenceInMinutes(this.targetDate, now) % 60;
-      this.seconds = differenceInSeconds(this.targetDate, now) % 60;
-    } else {
-      this.resetCountdown();
-    }
-    this.cdr.detectChanges(); // Agrega esto para que el cambio se detecte y se actualice la vista
+  async updateCountdown() {
+    this.intervalId = setInterval(
+      () => {
+        if (this.destroyComponent) {
+          clearInterval(this.intervalId);
+        }
+        else {
+          const now = new Date();
+          const distance = this.targetDate.getTime() - now.getTime();
+          if (distance > 0) {
+            this.days = differenceInDays(this.targetDate, now);
+            this.hours = differenceInHours(this.targetDate, now) % 24;
+            this.minutes = differenceInMinutes(this.targetDate, now) % 60;
+            this.seconds = differenceInSeconds(this.targetDate, now) % 60;
+          } else {
+            this.resetCountdown();
+          }
+        }
+      },
+      1000
+    );
+    //this.cdr.detectChanges();
   }
 
   resetCountdown() {
