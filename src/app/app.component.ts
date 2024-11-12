7f6955d1102616b77c7d1a2229356a79b7fa0145
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-11-11 23:32:21 -0600

    buttons_fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-// import { SimpleComponent } from './simple/simple.component';
-import { CountdownComponent } from './countdown-component/countdown-component.component';
-import { LocationMapComponent } from './location-map/location-map.component';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { CountdownComponent } from './countdown-component/countdown-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CountdownComponent, LocationMapComponent],
+  imports: [CommonModule, ReactiveFormsModule, CountdownComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Fecha';
-  
-}
-function scheduleReminder() {
-    
-}
+  attendanceForm: FormGroup;
+  showBankingInfo = false;
 
-function seeLocation(){
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.attendanceForm = this.fb.group({
+      attendance: ['', Validators.required],
+      nombre: ['', Validators.required],
+      mensaje: ['']
+    });
+  }
 
-}
-function verDatos(){
+  toggleBankingInfo(): void {
+    this.showBankingInfo = !this.showBankingInfo;
+  }
 
-}
-function SendReply(){
+  openInGoogleMaps(): void {
+    const url = `https://maps.app.goo.gl/4ieSj8TJUhregHtp9`;
+    window.open(url, '_blank');
+  }
 
+  onSubmit(): void {
+    const url = `https://forms.gle/eqvdCrytRSmGkyGv8`;
+    window.open(url, '_blank');
+  }
 }
