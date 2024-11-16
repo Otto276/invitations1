387f26cae69c8eaefd44a87e28fcc0a634f6026b
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2024-11-16 12:15:56 -0600

    countDownFix

--- src/app/countdown-component/countdown-component.component.ts
+++ src/app/countdown-component/countdown-component.component.ts
@@ -1,5 +1,7 @@
-import{ Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { differenceInDays, differenceInHours, differenceInMinutes, differenceInSeconds } from 'date-fns';
+
 
 @Component({
   selector: 'app-countdown',
@@ -14,47 +16,55 @@ export class CountdownComponent implements OnInit, OnDestroy {
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
-  private destroyComponent: boolean = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
-    this.updateCountdown().then(()=>{});
+    this.startCountdown();
   }
 
   ngOnDestroy() {
-    clearInterval(this.intervalId);
-    this.destroyComponent = true;
+    this.clearInterval();
   }
 
-  async updateCountdown() {
-    this.intervalId = setInterval(
-      () => {
-        if (this.destroyComponent) {
-          clearInterval(this.intervalId);
-        }
-        else {
-          const now = new Date();
-          const distance = this.targetDate.getTime() - now.getTime();
-          if (distance > 0) {
-            this.days = differenceInDays(this.targetDate, now);
-            this.hours = differenceInHours(this.targetDate, now) % 24;
-            this.minutes = differenceInMinutes(this.targetDate, now) % 60;
-            this.seconds = differenceInSeconds(this.targetDate, now) % 60;
-          } else {
-            this.resetCountdown();
-          }
-        }
-      },
-      1000
-    );
-    //this.cdr.detectChanges();
+  startCountdown() {
+    if (!isPlatformBrowser(this.platformId)) {
+      return; // No ejecuta el intervalo en el servidor
+    }
+
+    this.updateCountdown(); // Llamada inicial para calcular valores inmediatamente
+    this.intervalId = setInterval(() => this.updateCountdown(), 1000);
   }
+
+  updateCountdown() {
+    const now = new Date();
+    const distance = this.targetDate.getTime() - now.getTime();
+
+    if (distance > 0) {
+      this.days = differenceInDays(this.targetDate, now);
+      this.hours = differenceInHours(this.targetDate, now) % 24;
+      this.minutes = differenceInMinutes(this.targetDate, now) % 60;
+      this.seconds = differenceInSeconds(this.targetDate, now) % 60;
+    } else {
+      this.resetCountdown();
+    }
+  }
+
   resetCountdown() {
     this.days = this.hours = this.minutes = this.seconds = 0;
-    clearInterval(this.intervalId);
+    this.clearInterval();
   }
 
   setTargetDate(newDate: Date) {
     this.targetDate = newDate;
-    this.updateCountdown();
+    this.clearInterval();
+    this.startCountdown();
+  }
+
+  clearInterval() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 }
