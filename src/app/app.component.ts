c1b1fc4ce1ea68114f670666b732049e56ce3416
Otto276 <lopezgutierrezluisoctavio@gmail.com>
2025-09-02 13:20:14 -0600

    XVinvitation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { CountdownComponent } from './countdown-component/countdown-component.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,15 +17,14 @@ export class AppComponent {
   attendanceForm: FormGroup;
   showBankingInfo = false;
 
-  constructor(
-    private fb: FormBuilder,
-  ) {
+  constructor(private fb: FormBuilder) {
     this.attendanceForm = this.fb.group({
       attendance: ['', Validators.required],
       nombre: ['', Validators.required],
       mensaje: ['']
     });
   }
+
   startMusic(shouldPlay: boolean) {
     this.showModal = false;
     if (shouldPlay) {
@@ -32,17 +32,31 @@ export class AppComponent {
       audioPlayer?.play();
     }
   }
+
   toggleBankingInfo(): void {
     this.showBankingInfo = !this.showBankingInfo;
   }
 
   openInGoogleMaps(): void {
-    const url = `https://maps.app.goo.gl/4ieSj8TJUhregHtp9`;
+    const url = `https://maps.app.goo.gl/Zn3LuBtHbmfjgjmi9?g_st=aw`;
+    window.open(url, '_blank');
+  }
+
+  openInGoogleMapsD(): void {
+    const url = `https://maps.app.goo.gl/zVTSDEaNg4bAQEDR9`;
     window.open(url, '_blank');
   }
 
   onSubmit(): void {
     const url = `https://forms.gle/eqvdCrytRSmGkyGv8`;
+    window.open(url, '_blank');
+  }
+
+  // 👉 Nuevo método para abrir WhatsApp
+  openWhatsApp(): void {
+    const phoneNumber = '5213325520631'; // cambia por tu número en formato internacional
+    const message = encodeURIComponent('¡Hola! Me gustaría confirmar mi asistencia.');
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
     window.open(url, '_blank');
   }
 }
